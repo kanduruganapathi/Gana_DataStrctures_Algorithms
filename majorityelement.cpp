afb05b8ac7c42e0fe2a24566c07f4a95a8af09be
Kanduru Ganapathi <46512319+kanduruganapathi@users.noreply.github.com>
2024-02-23 10:16:57 +0530

    majorityelement.cpp

---
+++ majorityelement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        map<int,int> map;
+        for(int i=0;i<nums.size();i++)
+        {
+            map[nums[i]]++;
+        }
+        for(auto it:map)
+        {
+            if(it.second>nums.size()/2)
+            {
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
