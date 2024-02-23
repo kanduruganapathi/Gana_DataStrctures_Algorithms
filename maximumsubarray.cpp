5c84c1bf608fdb3bd2e064c5643262ff8fe39677
Kanduru Ganapathi <46512319+kanduruganapathi@users.noreply.github.com>
2024-02-23 11:35:38 +0530

    maximumsubarray.cpp

---
+++ maximumsubarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi=INT_MIN;
+        int s=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            s+=nums[i];
+            if(s>maxi)
+            {
+                maxi=s;
+            }
+            if(s<0)
+            {
+                s=0;
+            }
+        }
+        return maxi;
+        
+    }
+};
